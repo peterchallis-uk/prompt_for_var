8922c350c38588a6593c4ff6ea92271c6076ca62
PeteC <peter.challis@gmail.com>
2025-10-20 14:29:20 +0100

    Update index.ts

--- my-action/src/index.ts
+++ my-action/src/index.ts
@@ -2,10 +2,14 @@ import * as core from '@actions/core';
 
 async function run(): Promise<void> {
   try {
-    const message = core.getInput('message');
+    const message: string = core.getInput('message');
     console.log(`You entered: ${message}`);
   } catch (error) {
-    core.setFailed(`Action failed: ${error}`);
+    if (error instanceof Error) {
+      core.setFailed(error.message);
+    } else {
+      core.setFailed('Unknown error occurred');
+    }
   }
 }
 
